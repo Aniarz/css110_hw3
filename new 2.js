3c4fef5a4972fa460678173b1e71233088cdf8c9
Gor Arzumanyan <aniarzumanyan@ymail.com>
2016-09-25 12:26:43 -0700

    Add files via upload

---
+++ new 2.js
@@ -0,0 +1,18 @@
+var numbers = [0, 1, 2, 3, 4] 
+for (var i = 0; i <= numbers.length; i++) { 
+    if (numbers [i] == 0) { 
+        document.write(" * </br>"); 
+    } 
+    if (numbers [i] == 1) {
+        document.write(" *** </br>"); 
+    }
+    if (numbers [i] == 2) {
+        document.write(" ****** </br>"); 
+    } 
+    if (numbers [i] == 3) {
+        document.write(" ******* </br>"); }
+    if (numbers [i] == 4) { 
+        document.write("********* </br>"); 
+    }
+    return 
+}
